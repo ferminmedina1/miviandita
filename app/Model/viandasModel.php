1a5d2dda438cfaca38b13d3149b3cc6d4e133ea3
Fermin Medina <ferminmedina2002@gmail.com>
2020-09-23 18:05:28 -0300

    Model actualizado

--- app/Model/viandasModel.php
+++ app/Model/viandasModel.php
@@ -4,16 +4,53 @@
 class viandasModel{
     private $db;
 
-    public function getDataBase()
-    {
-        $this -> db = new PDO('mysql:host=localhost;'.'dbname=db_movies;charset=utf8', 'root', '');
+
+    function __construct() { //Preguntar sobre esto
+        // 1. Abro la conexión
+       $this->db = $this->connect();//Preguntar sobre esto
+   }
+
+    private function connect(){
+        // Conecta a la DB
+        $db = new PDO('mysql:host=localhost;'.'dbname=db_miviandita;charset=utf8', 'root', 'root');
+        return $db;
     }
 
     function getViandasByTipo($tipo) {
-        $query = $this->db->prepare('SELECT * FROM viandas WHERE genre = ?');
-        $query->execute([$tipo]); // array($genre)
+        $query = $this->db->prepare('SELECT * FROM viandas WHERE tipo = ?');
+        $query->execute([$tipo]); // array($tipo) PREGUNTAR SORBRE EL EXECUTE.
         $movies = $query->fetchAll(PDO::FETCH_OBJ);
         return $movies;
+    }
+
+    function getTipos(){ 
+        // DISTINCT no trae elementos repetidos. En este caso traera todos los generos existentes.
+        $query = $this->db->prepare('SELECT DISTINCT tipo FROM viandas'); 
+        $query->execute(); 
+        $tipos = $query->fetchAll(PDO::FETCH_OBJ);
+        return $tipos;
+    } 
+    
+    function getViandas() {
+        $query = $this->db->prepare('SELECT * FROM viandas');
+        $query->execute();
+        $viandas = $query->fetchAll(PDO::FETCH_OBJ);
+        return $viandas;
+    }
+    
+    function insertVianda($nombre,$tipo,$celiacos){
+        $query = $this->db->prepare('INSERT INTO viandas(nombre,tipo,celiacos) VALUES (?,?,?)');
+        $query->execute([$nombre,$tipo,$dietetico]);
+    }
+
+    function deleteVianda($id){
+        $sentencia = $this->db->prepare("DELETE FROM viandas WHERE id=?");
+        $sentencia->execute(array($id));
+    }
+
+    function marcarCeliaca($id){
+        $sentencia = $this->db->prepare("UPDATE viandas SET celiacos=1 WHERE id=?");
+        $sentencia->execute(array($id));
     }
 }
 
