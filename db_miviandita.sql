11c3dc09e4015cf07cd2ae1d448df1f247ca29f3
Agustin Arleo <arleo.agustin20@gmail.com>
2020-09-26 22:17:31 -0300

    se precargaron datos en la bd

--- db_miviandita.sql
+++ db_miviandita.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-09-2020 a las 00:08:55
+-- Tiempo de generación: 27-09-2020 a las 02:18:23
 -- Versión del servidor: 10.4.14-MariaDB
--- Versión de PHP: 7.4.10
+-- Versión de PHP: 7.4.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,8 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `dirigido_table` (
   `id_dirigidoA` int(11) NOT NULL,
-  `nombre-dirigido` varchar(288) NOT NULL
+  `tipo_vianda` varchar(288) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `dirigido_table`
+--
+
+INSERT INTO `dirigido_table` (`id_dirigidoA`, `tipo_vianda`) VALUES
+(6, 'Celiaca'),
+(7, 'Vegana'),
+(8, 'Normal');
 
 -- --------------------------------------------------------
 
@@ -42,9 +51,19 @@ CREATE TABLE `viandas` (
   `nombre` varchar(285) NOT NULL,
   `precio` int(11) NOT NULL,
   `id_dirigidoA` int(11) NOT NULL,
-  `celiacos` tinyint(1) NOT NULL,
+  `descripcion` varchar(800) NOT NULL,
   `id_vianda` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `viandas`
+--
+
+INSERT INTO `viandas` (`nombre`, `precio`, `id_dirigidoA`, `descripcion`, `id_vianda`) VALUES
+('Milanesa', 250, 8, 'Te presentamos la \"gran milanesa\", hecha con ingredientes de primera calidad. Este combo\r\nviene con una porcion de papas y una milanesa de carne para una persona, ideal para un buen almuerzo.', 4),
+('Ensalada vegana', 130, 7, 'La ensalada vegana es lo nuevo de la casa, tenemos opciones para todos los gustos. Incluye zanahoria, tomate, lechuga y rucula, no te quedes sin la tuya!!!', 5),
+('Fideos con salsa', 185, 8, 'Lo ideal para matar este frio es comer unos ricos fideos con salsa y como siempre en Mi viandita! tenemos para ofrecerte lo mejor. Para hacer tu pedido podes encontrarnos en la seccion \"contactos\".', 6),
+('Pizza', 290, 8, 'Es un plato de elaboración artesanal en la mayoría de los casos, aunque la industria alimentaria fabrica desde la década de 1950, pizzas preparadas total o parcialmente (prepizza) que se venden en diferentes comercios.', 7);
 
 --
 -- Índices para tablas volcadas
@@ -71,13 +90,13 @@ ALTER TABLE `viandas`
 -- AUTO_INCREMENT de la tabla `dirigido_table`
 --
 ALTER TABLE `dirigido_table`
-  MODIFY `id_dirigidoA` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_dirigidoA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `viandas`
 --
 ALTER TABLE `viandas`
-  MODIFY `id_vianda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_vianda` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Restricciones para tablas volcadas
